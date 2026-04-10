448cb64cee2201ca4ace1cdab833c916cdebed39
Pav <robsarmiento@icloud.com>
2026-04-10 08:52:25 -0500

    Question 1 Completed

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+        // Question 1:
+        double garySalary = 70000.21;
+        double bobSalary = 50000.10;
+        double highestSalary = Math.max(bobSalary,garySalary);
+
+        System.out.println("The highest salary is " + "$" + highestSalary);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
