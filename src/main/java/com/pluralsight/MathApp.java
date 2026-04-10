e794497edf8c3fe99ab09391bf6de8fc911b7d19
Pav <robsarmiento@icloud.com>
2026-04-10 09:08:42 -0500

    Question 2,3,4 Completed

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -9,6 +9,43 @@ public class MathApp {
 
         System.out.println("The highest salary is " + "$" + highestSalary);
 
+        //Question 2:
+
+        double carPrice = 49999.99;
+        double truckPrice = 79999.99;
+        double lowestPrice = Math.min(carPrice,truckPrice);
+
+        System.out.println("The cheapest vehicle is " + "$" + lowestPrice);
+
+        //Question 3:
+
+        double radius = 7.25;
+        double circleArea = Math.PI * Math.pow(radius, 2);
+
+        System.out.println("The area of a circle is " + circleArea);
+
+        //Question 4:
+
+        double numberVariable = 5.0;
+        double squareRoot = Math.sqrt(numberVariable);
+
+        System.out.println("the square root it " + squareRoot);
+
+
+        //Question 5:
+
+
+
+        //Question 6:
+
+
+
+        //Question 7:
+
+
+
+        //Question 8:
+
 
 
 
